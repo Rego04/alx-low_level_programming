b773f1761d298272a0aaee76745117d9015dd806
Super User <rneontlemokoena@gmail.com>
2023-02-21 11:52:23 -0800

    mains

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,15 +29,15 @@ void print_to_98(int n)
 	for (; n >= 98; n--)
 	{
 	if (n == 98)
-	{       
-        printf("%d", n);
-        printf("\n");
+	{
+		printf("%d", n);
+		printf("\n");
 	break;
-        }
-        else
-        {       
-        printf("%d, ", n);
-        }
-        }
-        }
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+	}
+	}
 }
