65d25694d228c567807ac955f5406e2d448632e5
Super User <rneontlemokoena@gmail.com>
2023-03-06 11:10:55 -0800

    Arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,9 +2,8 @@
 /**
  * print_chessboard - Entry point
  * @a: array
- * Return: 0
+ * Return: Always 0 (Success)
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i;
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
- * _putchar - puts character c to stdout
- * @c: The character being printed
- * Return: 1
- * On error, -1 is returned
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
